2a2c860708d1a08485f215fb47bed60618d52676
Zen <105056248+Ashborn1@users.noreply.github.com>
2026-06-27 01:57:49 +0800

    Update zenithhub.lua

--- zenithhub.lua
+++ zenithhub.lua
@@ -227,7 +227,7 @@ local TycoonTab = Window:CreateTab("Tycoon", "building")
 TycoonTab:CreateSection("Auto Upgrades")
 
 local autoBuyEnabled = false
-local loopDelay = 1
+local loopDelay = 2
 local upgradeTimes = 1 -- how many times to fire each prompt per loop
 
 -- All purchase folder names
@@ -317,7 +317,7 @@ TycoonTab:CreateSlider({
    Range = {1, 10},
    Increment = 0.5,
    Suffix = "s",
-   CurrentValue = 1,
+   CurrentValue = 2,
    Flag = "AutoBuyDelay",
    Callback = function(Value)
       loopDelay = Value
